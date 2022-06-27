d22d43ca9341a17f4fb1a9703ae1618304422e04
rgast <rgast@cbs.mpg.de>
2022-06-27 15:13:42 -0500

    added a new function that sorts adjacency matrices according to module dictionaries

--- pyrecu/__init__.py
+++ pyrecu/__init__.py
@@ -1,3 +1,3 @@
 from .readout import ReadoutTraining
 from .rnn import RNN
-from .util import sequentiality, modularity, circular_connectivity, random_connectivity
+from .util import sequentiality, modularity, circular_connectivity, random_connectivity, sort_via_modules
