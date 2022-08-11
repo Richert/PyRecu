73de7662e5380b9707c15ed774fb28ce0faf3f25
Richert <richgast.90@gmail.com>
2022-08-11 08:38:20 -0500

    removed pytorch-related code (exists as separate package called RectiPy now)

--- pyrecu/__init__.py
+++ pyrecu/__init__.py
@@ -1,4 +1,3 @@
 from .readout import ReadoutTraining
 from .rnn import RNN
 from .util import sequentiality, modularity, circular_connectivity, random_connectivity, sort_via_modules
-from .network import Network
